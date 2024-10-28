9e43cc92a46de9b9b60a70d6b9cd6b24e0a3466b
mohHafidz <114865846+daditerl@users.noreply.github.com>
2024-10-28 19:49:38 +0700

    ubah variable status

--- app/src/main/java/com/example/mitsubishi/AddCarActivity.kt
+++ app/src/main/java/com/example/mitsubishi/AddCarActivity.kt
@@ -141,7 +141,7 @@ class AddCarActivity : AppCompatActivity() {
                                 "noPolis" to nopol,
                                 "model" to model,
                                 "photos" to photoDataList,
-                                "status" to "false"
+                                "status" to false
                             )
                             db.collection("cars").document(carId)
                                 .set(carData)
